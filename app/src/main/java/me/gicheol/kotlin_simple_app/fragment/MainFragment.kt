98521c41509710b249e05907c987c7a793e7e015
LeeGiCheol <asdqwek200@naver.com>
2021-12-20 23:43:45 +0900

    kotlin simple app complete

---
+++ app/src/main/java/me/gicheol/kotlin_simple_app/fragment/MainFragment.kt
@@ -0,0 +1,40 @@
+package me.gicheol.kotlin_simple_app.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_main.*
+import me.gicheol.kotlin_simple_app.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [MainFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class MainFragment : Fragment() {
+
+    lateinit var navController: NavController
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        btn_next.setOnClickListener {
+            navController.navigate(R.id.action_mainFragment_to_questionFragment)
+        }
+    }
+
+}
